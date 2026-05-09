6ef17160f0141d17a472389a8ab25d98fea341e4
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-05-10 03:19:49 +0900

    Initial commit

---
+++ oop-team-project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oop_team_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
